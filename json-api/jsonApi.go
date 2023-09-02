650a7a11e88480bac802e19a45aa3b5436a599c3
JustUzair <uzairhajra763390@gmail.com>
2023-09-02 10:10:10 +0530

    Added minor logging details to server

--- json-api/jsonApi.go
+++ json-api/jsonApi.go
@@ -157,6 +157,7 @@ func Serve(db *sql.DB, bind string) {
 	http.Handle("/email/get_batch", GetEmailBatch(db))
 	http.Handle("/email/update", UpdateEmail(db))
 	http.Handle("/email/delete", DeleteEmail(db))
+	log.Printf("server listening on %v \n", bind)
 	err := http.ListenAndServe(bind, nil)
 	if err != nil {
 		log.Fatalf("json server error : %v\n", err)
