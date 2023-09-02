2f1f1c46d7c3393af6cdf15da5bc6854a836ccdd
JustUzair <uzairhajra763390@gmail.com>
2023-09-02 10:06:38 +0530

    Created server file to serve incoming requests

---
+++ server/server.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	jsonapi "mailinglist/json-api"
+	"mailinglist/mdb"
+	"sync"
+
+	arg "github.com/alexflint/go-arg"
+)
+
+var args struct {
+	DbPath   string `arg:"env:MAILINGLIST_DB"`
+	BindJson string `arg:"env:MAILINGLIST_BIND_JSON"`
+}
+
+func main() {
+	arg.MustParse(&args)
+	if args.DbPath == "" {
+		args.DbPath = "list.db"
+	}
+	if args.BindJson == "" {
+		args.BindJson = ":8080"
+	}
+
+	log.Printf("using database '%v'\n", args.DbPath)
+	db, err := sql.Open("sqlite3", args.DbPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+	mdb.TryCreate(db)
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		log.Printf("starting json api server...\n")
+		jsonapi.Serve(db, args.BindJson)
+	}()
+	wg.Wait()
+}
